5d3d20e8f7af9366c151161920d779cd0f96e311
damianrivas <drivas1993@gmail.com>
2018-08-26 15:19:42 -0700

    Add the marco polo route

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -11,4 +11,20 @@ describe("routes : static", () => {
       });
     });
   });
+
+  describe("GET /marco", () => {
+    it('should return "polo"', done => {
+      request.get(base + "marco", (err, res, body) => {
+        expect(body).toContain("polo");
+        done();
+      });
+    });
+
+    it("should return status code 200", done => {
+      request.get(base + "marco", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
 });
