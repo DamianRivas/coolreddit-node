e9665caa5231ac53ab2e74dc02cfff87ce202559
damianrivas <drivas1993@gmail.com>
2018-11-21 04:32:56 -0800

    Create spec for Topic model

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,45 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+
+describe("Topic", () => {
+  beforeEach(done => {
+    this.topic;
+
+    sequelize.sync({ force: true }).then(res => {
+      Topic.create({
+        title: "Expiditions to Alpha Centauri",
+        description:
+          "A compilation of reports from recent visits to the star system."
+      })
+        .then(topic => {
+          this.topic = topic;
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
+        });
+    });
+  });
+
+  describe("#create()", () => {
+    it("should create a topic object with a title and description", done => {
+      Topic.create({
+        title: "Everything Lions",
+        description: "Everything about the greatest creatures on Earth."
+      })
+        .then(topic => {
+          expect(topic.title).toBe("Everything Lions");
+          expect(topic.description).toBe(
+            "Everything about the greatest creatures on Earth."
+          );
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          fail("Failed to create a topic");
+          done();
+        });
+    });
+  });
+});
