7362ef6c2b800d32d9846c71b82afc3b65e98393
damianrivas <drivas1993@gmail.com>
2018-11-22 14:18:47 -0800

    Associate Users to Topics and Posts

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,20 +1,46 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("Topic", () => {
   beforeEach(done => {
     this.topic;
+    this.post;
+    this.user;
 
     sequelize.sync({ force: true }).then(res => {
-      Topic.create({
-        title: "Expiditions to Alpha Centauri",
-        description:
-          "A compilation of reports from recent visits to the star system."
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
       })
-        .then(topic => {
-          this.topic = topic;
-          done();
+        .then(user => {
+          this.user = user;
+
+          Topic.create(
+            {
+              title: "Expiditions to Alpha Centauri",
+              description:
+                "A compilation of reports from recent visits to the star system.",
+              posts: [
+                {
+                  title: "My first visit to Proxima Centauri b",
+                  body: "I saw some rocks.",
+                  userId: this.user.id
+                }
+              ]
+            },
+            {
+              include: {
+                model: Post,
+                as: "posts"
+              }
+            }
+          ).then(topic => {
+            this.topic = topic;
+            this.post = topic.posts[0];
+            done();
+          });
         })
         .catch(err => {
           console.log(err);
@@ -60,19 +86,17 @@ describe("Topic", () => {
 
   describe("#getPosts()", () => {
     it("should return an array of associated posts", done => {
-      Post.create({
-        title: "My first trip",
-        body: "I saw lots of stars",
-        topicId: this.topic.id
-      }).then(post => {
-        this.topic.getPosts().then(posts => {
+      this.topic
+        .getPosts()
+        .then(posts => {
           expect(posts.length).toBe(1);
-          post = posts[0];
-
-          expect(post.title).toBe("My first trip");
+          expect(posts[0].title).toBe(this.post.title);
+          done();
+        })
+        .catch(err => {
+          console.log(err);
           done();
         });
-      });
     });
   });
 });
